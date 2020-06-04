76050b87ced67ee7ef07280dad0e0913764887a3
Dahan Schuster <dan.plschuster@gmail.com>
2020-06-04 20:47:16 -0300

    Aplica a validação do formulário utilizando o Yup
    Utiliza o Yup e o React Hook useRef para validar o formulário ao clicar
    em cadastrar. Os erros são definidos no formulário através da função
    setErrors do useRef

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import * as Yup from 'yup';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Background, Content } from './styles';
@@ -7,27 +11,58 @@ import { Container, Background, Content } from './styles';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
-const SignUp: React.FC = () => (
-	<Container>
-		<Background />
-		<Content>
-			<img src={logoImg} alt="GoBarber" />
-			<form>
-				<h1>Faça seu cadastro</h1>
+import getValidationErros from '../../utils/getValidationErros';
 
-				<Input icon={FiUser} name="name" placeholder="Nome" />
-				<Input icon={FiMail} name="email" placeholder="E-mail" />
-				<Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+interface FormData {
+	name: string;
+	email: string;
+	password: string;
+}
 
-				<Button type="submit">Cadastrar</Button>
-			</form>
+const SignUp: React.FC = () => {
+	const formRef = useRef<FormHandles>(null);
 
-			<a href="">
-				<FiArrowLeft />
-				Voltar para o Logon
-			</a>
-		</Content>
-	</Container>
-);
+	const handleSubmit = useCallback(async (data: FormData) => {
+		try {
+			formRef.current?.setErrors({});
+
+			const schema = Yup.object().shape({
+				name: Yup.string().required('Nome obrigatório'),
+				email: Yup.string().required('Email obrigatório').email('Email inválido'),
+				password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+			});
+
+			await schema.validate(data, {
+				abortEarly: false,
+			});
+		} catch (err) {
+			const errors = getValidationErros(err);
+			formRef.current?.setErrors(errors);
+		}
+	}, []);
+
+	return (
+		<Container>
+			<Background />
+			<Content>
+				<img src={logoImg} alt="GoBarber" />
+				<Form ref={formRef} onSubmit={handleSubmit}>
+					<h1>Faça seu cadastro</h1>
+
+					<Input icon={FiUser} name="name" placeholder="Nome" />
+					<Input icon={FiMail} name="email" placeholder="E-mail" />
+					<Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+
+					<Button type="submit">Cadastrar</Button>
+				</Form>
+
+				<a href="">
+					<FiArrowLeft />
+					Voltar para o Logon
+				</a>
+			</Content>
+		</Container>
+	);
+};
 
 export default SignUp;
