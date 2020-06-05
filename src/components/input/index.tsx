d045801456f9ac8a763b4f9edfa11597aa50388e
Dahan Schuster <dan.plschuster@gmail.com>
2020-06-04 21:33:41 -0300

    Adiciona a exibição dos erros no componente Input
    Adiciona a propriedade isErrored ao styled componente do Input, que
    alica uma borda vermelha quando o mesmo possui um erro
    
    Adiciona um ícone de erro com um tooltip que contém a mensagem de erro
    Cria uma herança do estilo do componente Tooltip para erros, o
    TooltipError

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,21 +1,21 @@
 import React, { ComponentType, InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
-
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, TooltipError } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	name: string;
 	icon?: ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name: inputName, icon: Icon, ...rest }) => {
 	const [isFocused, setIsFocused] = useState(false);
 	const [isFilled, setIsFilled] = useState(false);
 
 	const inputRef = useRef<HTMLInputElement>(null);
-	const { fieldName, defaultValue, registerField } = useField(name);
+	const { fieldName, defaultValue, error, registerField } = useField(inputName);
 
 	useEffect(() => {
 		registerField({
@@ -35,7 +35,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
 	}, []);
 
 	return (
-		<Container isFocused={isFocused} isFilled={isFilled}>
+		<Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
 			{Icon && <Icon size={20} />}
 			<input
 				{...rest}
@@ -44,6 +44,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
 				onFocus={handleInputFocus}
 				onBlur={handleInputBlur}
 			/>
+
+			{error && (
+				<TooltipError title={error}>
+					<FiAlertCircle color="#c53030" size={20} />
+				</TooltipError>
+			)}
 		</Container>
 	);
 };
--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../tooltip';
 
 interface ContainerProps {
 	isFocused: boolean;
 	isFilled: boolean;
+	isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -17,6 +19,12 @@ export const Container = styled.div<ContainerProps>`
 	svg {
 		margin-right: 16px;
 	}
+
+	${(props) =>
+		props.isErrored &&
+		css`
+			border-color: #c53030;
+		`}
 
 	${(props) =>
 		props.isFocused &&
@@ -46,6 +54,24 @@ export const Container = styled.div<ContainerProps>`
 
 		::placeholder {
 			color: #666360;
+		}
+	}
+`;
+
+export const TooltipError = styled(Tooltip)`
+	height: 20px;
+	margin-left: 16px;
+
+	svg {
+		margin: 0;
+	}
+
+	span {
+		background: #c53030;
+		color: #fff;
+
+		&:before {
+			border-color: #c53030 transparent;
 		}
 	}
 `;
