965241de5b708454a82e8244b00e0cb965d2522d
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-26 12:03:57 -0300

    Implementação da recuperação de senha
    Adiciona uma propriedade ao componente Button, informando se está em
    estado de loading. Caso positivo, mostra um componente ReactLoading ao
    invés da propriedade children

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import ReactLoading from 'react-loading';
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
 	<Container>
-		<button type="button" {...rest}>
-			{children}
+		<button disabled={!!loading} type="button" {...rest}>
+			{loading ? <ReactLoading type="bubbles" color="#000" /> : children}
 		</button>
 	</Container>
 );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -13,9 +13,17 @@ export const Container = styled.div`
 		font-weight: 500;
 		margin-top: 16px;
 		transition: background 0.2s;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
 		&:hover {
 			background: ${shade(0.2, '#ff9000')};
+		}
+
+		&:disabled {
+			cursor: not-allowed;
+			background-color: ${shade(0.2, '#ff9000')};
 		}
 	}
 `;
