c81390011225b81fd52d3c97b8a17ab725162363
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-27 10:48:49 -0300

    Implementação da tela de Dashboard
    Implementa o DayPicker na tela de dashboard e aplica um estilo
    personalizado nele. Neste ponto do desenvolvimento o calendário está sem
    conexão à API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FiClock } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import {
 	Container,
@@ -17,6 +19,12 @@ import defaultAvatar from '../../assets/default-avatar.svg';
 
 const Dashboard: React.FC = () => {
 	const [selectedDate, setSelectedDate] = useState(new Date());
+
+	const handleDateChange = useCallback((day, modifiers: DayModifiers) => {
+		if (modifiers.available) {
+			setSelectedDate(day);
+		}
+	}, []);
 
 	return (
 		<Container>
@@ -100,7 +108,32 @@ const Dashboard: React.FC = () => {
 						</Appointment>
 					</Section>
 				</Schedule>
-				<Calendar />
+				<Calendar>
+					<DayPicker
+						weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+						fromMonth={new Date()}
+						disabledDays={[{ daysOfWeek: [0, 6] }]}
+						modifiers={{
+							available: { daysOfWeek: [1, 2, 3, 4, 5] },
+						}}
+						selectedDays={selectedDate}
+						onDayClick={handleDateChange}
+						months={[
+							'Janeiro',
+							'Fevereiro',
+							'Março',
+							'Abril',
+							'Maio',
+							'Junho',
+							'Julho',
+							'Agosto',
+							'Setembro',
+							'Outubro',
+							'Novembro',
+							'Dezembro',
+						]}
+					/>
+				</Calendar>
 			</Content>
 		</Container>
 	);
