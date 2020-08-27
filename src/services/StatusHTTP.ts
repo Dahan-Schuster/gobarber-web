f07a75371d494fb303c9d323d94bee9d7b8dcbdc
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-27 11:52:28 -0300

    Implementação da tela de Dashboard
    Cria o objeto StatusHTTP que contém os principais códigos retornados
    pela API

---
+++ src/services/StatusHTTP.ts
@@ -0,0 +1,14 @@
+/**
+ * Object StatusHTTP
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+const StatusHTTP = {
+	SUCCESS: 200,
+	BAD_REQUEST: 400,
+	TOO_MANY_REQUESTS: 429,
+	INTERNAL_SERVER_ERROR: 500,
+};
+
+export default StatusHTTP;
